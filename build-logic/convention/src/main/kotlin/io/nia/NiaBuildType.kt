d14382c2caf062675f0a95e67edf0b5522419a93
TomokiHamada <t.hamada@fenrir.co.jp>
2024-09-10 01:14:47 +0900

    ✨ feat: Convention Pluginを導入

---
+++ build-logic/convention/src/main/kotlin/io/nia/NiaBuildType.kt
@@ -0,0 +1,9 @@
+package io.nia
+
+/**
+ * This is shared between :app and :benchmarks module to provide configurations type safety.
+ */
+enum class NiaBuildType(val applicationIdSuffix: String? = null) {
+    DEBUG(".debug"),
+    RELEASE,
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.exclude
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
-   alias(libs.plugins.android.application) apply false
-   alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.room) apply false
 }
